d00a30b02218a97a128d229c26e4ad29c81b2a4a
Ezekiel Aquino <ezekielaquino@gmail.com>
2017-03-03 12:12:12 +0100

    🎉  Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# SEED3000
+
+Start simple™
+
+Super simple starter kit for web stuff ✨
+
+
+Note: This is my personal "getting started" package for things I build on the web. Covers basically what you need to get something going– an experiment, prototype, a single page site, or the beginnings of something grand. I hope you find this useful! Enjoy.
+
+## Features
+- Basic setup to get you started!
+- No frills
+- Runs on gulp (npm is too npm for my taste)
+- Compiles stylus, twig
+- Compresses images
+- Clean and easy media queries via Rupture
+- Autoprefix, because yolo
+- ES6 support
+- Live reload
+- Module up with Webpack
+- Cute log messages with emojis (lol)
+
+More to follow
+
+
+## Usage
+
+A) Install yarn (https://yarnpkg.com/lang/en/) `npm install -g yarn`
+
+1. Clone this repo
+
+2. `cd` to wherever you cloned it then run `yarn` to install dependencies
+
+3. Run `npm run dev` (there's a welcome message)
+
+4. Start coding
+
+
+## Say hi!
+The is completely free but I'd love to know if you have found it useful or improved it in any way! Drop me a line at ezekielaquino@gmail.com or via @the_ezekiel on Twitter!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
+var rename = require('gulp-rename');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var twig = require('gulp-twig');
+var stylus = require('gulp-stylus');
+var rupture = require('rupture');
+var autoprefixer = require('autoprefixer-stylus');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var replace = require('gulp-replace');
+var htmlreplace = require('gulp-html-replace');
+
+var paths = {
+  html: './source/twig/**/*.html',
+  stylus: './source/stylus/**/*.styl',
+  images: './source/images/**/*'
+}
+
+var dest = {
+  build: './',
+  assets: './assets',
+  images: './assets/img'
+}
+
+gulp.task('compile-html', function() {
+  return gulp.src(paths.html)
+    .pipe(twig())
+    .pipe(gulpif(argv.prod, htmlmin({ collapseWhitespace: true })))
+    .pipe(gulpif(argv.prod, replace('/source/images/', '/assets/img')))
+    .pipe(gulpif(argv.prod, htmlreplace({ css: 'style.min.css' })))
+    .pipe(gulp.dest(dest.build))
+
+  .on('end', function() {
+    log('🏄 Html: Done!');
+    if (argv.prod) log('🐳 Html: Minified!');
+  });
+});
+
+
+gulp.task('compile-stylus', function() {
+  var options = {
+    use: [rupture(), autoprefixer()],
+    compress: argv.prod ? true : false
+  };
+
+  return gulp.src('./source/stylus/style.styl')
+    .pipe(stylus(options))
+    .pipe(gulpif(argv.prod, rename('style.min.css')))
+    .pipe(gulpif(argv.prod, replace('/source/images/', '/assets/img/')))
+    .pipe(gulp.dest('./'))
+
+  .on('end', function() {
+    log('🍒 Style: Done!');
+    if (argv.prod) log('🍭 Style: Minified!');
+  });
+});
+
+
+gulp.task('compress-images', function() {
+  return gulp.src(paths.images)
+    .pipe(imagemin())
+    .pipe(gulp.dest(dest.images))
+
+  .on('end', function() {
+    log('📸 Images: Compressed!')
+  });
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(paths.html, ['compile-html']);
+  gulp.watch(paths.stylus, ['compile-stylus']);
+});
+
+
+gulp.task('dev', [
+  'compile-html',
+  'compile-stylus',
+  'watch'
+]);
+
+gulp.task('prod', [
+  'compress-images',
+  'compile-html',
+  'compile-stylus'
+]);
+
+
+function log(message) {
+  gutil.log(gutil.colors.bold.green(message));
+}
